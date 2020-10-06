95f33d5cce58e575e0efcb0d562ed347c633e76b
ilia98naidenov <ilya.naydenov@mail.ru>
2020-10-06 09:41:17 +0300

    The required direct-modeling data form is introduced.

--- project/test_dir_inv/ert_test.py
+++ project/test_dir_inv/ert_test.py
@@ -5,6 +5,13 @@ import matplotlib.pyplot as plt
 from pygimli import meshtools as mt
 from pygimli.physics import ert
 
+l = 5                          # ДЛИНА КОСЫ
+L = 10                         # ДЛИНА ПРОФИЛЯ
+h = 0.1                        # ШАГ М/У ЭЛЕКТРОДАМИ
+N_l = int(l/h + 1)             # КОЛ-ВО ЭЛЕКТРОДОВ В КОСЕ
+N_L = int(L/h + 1)             # КОЛ-ВО ЭЛЕКТРОДОВ НА ПРОФИЛЕ
+x0 = 0                         # НАЧАЛО ПРОФИЛЯ ИЗМЕРЕНИЙ (НЕ ДОЛЖЕН ВЫЛЕЗАТЬ ЗА ПРЕДЕЛЫ МОДЕЛИ!)
+
 world = mt.createWorld(start=[-5,0],end=[15,-10],layers=[-1.5,-3.2])
 
 #ДЕЛАЕМ КРИВУЮ ДЛЯ CREATE_POLYGON
@@ -24,7 +31,7 @@ pg.show(model)
 print(type(model))
 
 # СХЕМА ДИПОЛЬ-ДИПОЛЬ, расст. м/у эл. = 100 м
-scheme = ert.createERTData(elecs=np.linspace(start=0,stop=10,num=21),schemeName='dd')
+scheme = ert.createERTData(elecs=np.linspace(start=x0,stop=x0+L,num=N_L),schemeName='dd')
 
 # СДЕЛАЛИ ДИСКРЕТИЗАЦИЮ БУДУЩЕЙ СЕТКИ КАК 10% РАССТОЯНИЯ М/У ЭЛЕКТРОДАМИ, ДЛЯ НОРМ КАЧЕСТВА
 for p in scheme.sensors():
@@ -38,8 +45,10 @@ rhomap = [[0,75],[1,50],[2,125],[3,200],[4,200],[5,300]]
 # ВОТ КАК ОНА ВЫГЛЯДИТ:
 pg.show(mesh,data=rhomap,label=pg.unit('res'),showMesh=True)
 
+mesh.save('mesh')
+
 # МОДЕЛИРУЕМ ДАННЫЕ ПРЯМОЙ ЗАДАЧИ, С ШУМОМ
-data = ert.simulate('mesh_test.bms',scheme=scheme,res=rhomap,noiseLevel=1,noiseAbs=1e-6,seed=1337)
+data = ert.simulate('mesh.bms',scheme=scheme,res=rhomap,noiseLevel=1,noiseAbs=1e-6,seed=1337)
 
 # ЭТО ПОКА ХЗ ЗАЧЕМ
 pg.warning(np.linalg.norm(data['err']), np.linalg.norm(data['rhoa']))
@@ -52,19 +61,21 @@ pg.info('Selected data noise %(min/max)', min(data['err'])*100, max(data['err'])
 # ФИЛЬТРУЕМ УЧАСТКИ С rho < 0:
 data.remove(data['rhoa'] < 0)
 pg.info('Filtered rhoa (min/max)', min(data['rhoa']), max(data['rhoa']))
-data.save('ERT_test.dat') # СОХРАНИМ ЕЖЖЕ
+data.save('origin_data.dat')
+data.save('required_data.dat')
 
-# ПОКАЗЫВАЕМ РЕЗУЛЬТАТ ПРЯМОЙ ЗАДАЧИ
+# ПОКАЗЫВАЕМ РЕЗУЛЬТАТ ПРЯМОЙ ЗАДАЧИ ДО ОБРАБОТКИ
 ert.show(data)
 
-# ЗАПУСКАЕМ КАКОЙ ТО ЕРТ МЕНЕЖЕР
-mng = ert.ERTManager('ERT_test.dat')
+# ЗАГРУЖАЕМ КОПИЮ ТАБЛИЧКИ
+data1 = pg.load('required_data.dat') 
 
-# ИНВЕРСИРУЕМ
-inv = mng.invert(lam=10,verbose=True)
+# ПРОЧЕСЫВАЕМ ТАБЛИЧКУ .DAT, ЧТОБЫ ОБРЕЗАТЬ ТРЕУГОЛЬНИК ДО ТРАПЕЦИИ. 
+# РАССТОЯНИЕ МЕЖДУ 'a' И 'n' ЭЛЕКТРОДАМИ НЕ ДОЛЖНО ПРЕВЫШАТЬ ДЛИНУ КОСЫ (N)
 
-# РЕЗУЛЬТАТ
-mng.showResultAndFit()
-meshPD = pg.Mesh(mng.paraDomain) # делаем копию инверсионных данных для графика
-inv.save('invert')
-pg.show(mng.paraDomain)
+for i in range(len(data['valid'])):
+    if data1['n'][i] - data['a'][i] > N_l-1:
+        data1['valid'][i] = 0
+
+# РЕЗУЛЬТАТ ОБРАБОТКИ:
+ert.show(data1)
