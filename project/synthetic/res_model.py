23c580b4788e7eb0505390832c8496ad98ddc7ac
at-dl <100daloro100@gmail.com>
2020-10-16 08:33:32 +0300

    comments added

--- project/synthetic/res_model.py
+++ project/synthetic/res_model.py
@@ -9,51 +9,97 @@ import geopandas as gpd
 
 
 ResType = Union[float, np.ndarray]
-PointType = Union[Tuple[float, float], np.ndarray]
+PointType = Union[Tuple[float, float], np.ndarray]  # point coordinates sorted in (x, y) order
 
 
 class ModelConfig:
+    """
+    A class with parameter limits for random generation.
+    Also contains the geometrical dimensions of the world.
+    """
     class ResValues:
+        """
+        Resistance limits for all subsequent classes.
+        """
         min_res = 10
         max_res = 10000
 
     class World(ResValues):
+        """
+        Model world/background/underlay with dimensions.
+        For convenience of calculation, the depth ("bottom") is positive, but at the last step of combining the
+        primitives, the depths become negative.
+        """
         left = 0
         top = 0
         right = 1000
         bottom = 200
 
     class HorizontalLayer(ResValues):
+        """
+        Simple horizontal layer
+        height: thickness of layer in orthogonal direction
+        depth: depth of top of layer
+        """
         min_height = 5
         max_height = 100
         min_depth = 5
         max_depth = 50
 
     class InclinedLayer(HorizontalLayer):
+        """
+        Inclined layer of constant thickness.
+        depth: the topmost point of the layer, which is on the far left or right of the model
+        angle: layer tilt angle. May be negative
+        """
         min_angle = 0
         max_angle = 30
 
     class CurvedLayer(InclinedLayer):
+        """
+        Inclined layer with a bulge.
+        height_curve: the height of the bulge
+        width_curve: the width of the bulge
+        """
         min_height_curve = 5
         max_height_curve = 15
         min_width_curve = 10
         max_width_curve = 100
 
     class Wedge(ResValues):
+        """
+        Basic class for wedges.
+        depth: the topmost point of the object, which is on the far left or right of the model
+        """
         min_depth = 1
         max_depth = 15
 
     class InnerWedge(Wedge, ResValues):
+        """
+        Wedge, the corner of which is inside the model.
+        height: layer thickness at the border of the model.
+                The thickness gradually decreases from this value at the edge of the model to zero at the corner of
+                the wedge.
+        width: distance between the border of the model and the corner of the wedge.
+        """
         min_height = 10
         max_height = 40
         min_width = 40
         max_width = 300
 
     class OuterWedge(Wedge, ResValues):
+        """
+        Wedge, the corner of which is outside the model.
+        height: layer thickness at the border of the model.
+                The thickness gradually changes from one value at one border to another value at the other border.
+        """
         min_height = 5
         max_height = 100
 
     class Lens(ResValues):
+        """
+        The lens represents an object that looks like a truncated ellipse.
+        """
         min_height = 5
         max_height = 20
         min_width = 10
@@ -62,6 +108,11 @@ class ModelConfig:
         max_depth = 40
 
     class LayersContact(CurvedLayer):
+        """
+        Composite transformation of multiple layers, connecting two layers of different resistance.
+        height: the thickness of the layers to be joined
+        contact_angle: angle of inclination of the contact surface
+        """
         min_height = 10
         max_height = 40
         min_contact_angle = 20
@@ -85,6 +136,26 @@ class ResObject:
                  resistivity: Optional[ResType] = None,
                  marker: Optional[int] = None,
                  *args, **kwargs):
+        """
+        Base class for generating objects.
+        Contains abstract methods for working with polygons, point validation, and also contains helper methods
+        for geometric calculations.
+
+        Class allow you create geometric objects with specified extra parameters.
+        If some parameters are not specified, random values will be generated for them,
+        taken from the configuration class.
+
+        Arguments:
+            resistivity: Resistance values for an object. If not specified, it will be generated randomly.
+            marker: A number that will later be assigned the resistance value for all such numbers.
+
+        Attributes:
+            resistivity: see Arguments.
+            marker: see Arguments.
+            polygon: polygon (enclosed area) is a geometric representation of a geoelectric feature.
+            It can be represented as a collection of points.
+            random: function that generates random values in a given range.
+        """
         if resistivity is None:
             self.resistivity = rand.randrange(ModelConfig.ResValues.min_res, ModelConfig.ResValues.max_res)
         else:
@@ -99,6 +170,10 @@ class ResObject:
         self.random = np.random.uniform
 
     def _gen_value_if_need(self, min_value: float, max_value: float, curr_value: float, include_negative: bool = False):
+        """
+        Method check that "curr_value" is None. If True it generates random value between "min_value" and "max_value",
+        including negative ones if "include_negative" is True. Otherwise, it returns the "curr_value".
+        """
         if curr_value is None:
             if include_negative:
                 values = [self.random(min_value, max_value), self.random(-max_value, -min_value)]
@@ -108,13 +183,19 @@ class ResObject:
         else:
             return curr_value
 
-    def _construct_mesh(self):
+    def _construct_polygon(self):
+        """
+        An abstract method that must be called at the end of the "__init__" to build the object's polygon.
+        """
         pass
 
     def get_polygon(self):
         return self.polygon
 
     def set_marker(self, marker: int):
+        """
+        The method allows you to change or assign a marker after creating an object.
+        """
         self.marker = marker
 
     def __call__(self):
@@ -148,6 +229,9 @@ class ResObject:
 
     @classmethod
     def get_world_pts(cls):
+        """
+        The method returns a set of corner points that constrain the dimensions of the model.
+        """
         return [[ModelConfig.World.left, ModelConfig.World.top],
                 [ModelConfig.World.right, ModelConfig.World.top],
                 [ModelConfig.World.right, -ModelConfig.World.bottom],
@@ -162,14 +246,17 @@ class ResObject:
             return False
 
     @classmethod
-    def _calc_opposite_point_with_angle(cls, depth: float, pt: PointType, angle: float):
-        assert ModelConfig.World.top <= depth <= ModelConfig.World.bottom
+    def _calc_opposite_point_with_angle(cls, pt: PointType, angle: float):
+        """
+        Finds a point on a boundary (left, right or bottom) that lies on a ray passing at a specified "angle" from
+        the point "pt".
+        """
         assert cls._check_point_inside_model(pt)
         assert -90 <= angle <= 90
 
         dx = 1
         dz = - np.tan(np.deg2rad(angle)) * dx
-
+        # Extra point on same ray
         delta_pt = (pt[0] + dx, pt[1] + dz)
 
         bottom_intersection = cls._get_intersection_point(pt, delta_pt, *cls.bottom_border_pts)
@@ -202,9 +289,9 @@ class World(ResObject):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self._construct_mesh()
+        self._construct_polygon()
 
-    def _construct_mesh(self):
+    def _construct_polygon(self):
         self.polygon = Polygon(self.get_world_pts())
 
 
@@ -213,13 +300,18 @@ class HorizontalLayer(ResObject):
                  depth: Optional[float] = None,
                  height: Optional[float] = None,
                  *args, **kwargs):
+        """
+        Class allow you create horizontal layer with specified parameters.
+
+        See "args" and "kwargs" description in parent class.
+        """
         super().__init__(*args, **kwargs)
         self.depth = depth
         self.height = height
 
-        self._construct_mesh()
+        self._construct_polygon()
 
-    def _construct_mesh(self):
+    def _construct_polygon(self):
         depth = self._gen_value_if_need(ModelConfig.HorizontalLayer.min_depth,
                                         ModelConfig.HorizontalLayer.max_depth,
                                         self.depth)
@@ -242,14 +334,23 @@ class InclinedLayer(ResObject):
                  height: Optional[float] = None,
                  angle: Optional[float] = None,
                  *args, **kwargs):
+        """
+        Class allow you create inclined layer with specified parameters.
+
+        See "args" and "kwargs" description in parent class.
+        """
         super().__init__(*args, **kwargs)
         self.depth = depth
         self.height = height
         self.angle = angle
 
-        self._construct_mesh()
+        self._construct_polygon()
 
-    def _construct_mesh(self):
+    def _construct_polygon(self):
+        """
+        The method builds a polygon by calculating the coordinates of the top and bottom lines of the layer.
+        Since the layer is infinite, it touches one of the boundaries higher than the other.
+        """
         depth = self._gen_value_if_need(ModelConfig.InclinedLayer.min_depth,
                                         ModelConfig.InclinedLayer.max_depth,
                                         self.depth)
@@ -263,7 +364,7 @@ class InclinedLayer(ResObject):
 
         if 0 <= angle <= 90:
             upper_line_pt1 = (ModelConfig.World.right, depth)
-            upper_line_pt2 = self._calc_opposite_point_with_angle(depth, upper_line_pt1, angle)
+            upper_line_pt2 = self._calc_opposite_point_with_angle(upper_line_pt1, angle)
             bottom_value = depth + height / np.cos(np.deg2rad(angle))
 
             if bottom_value > ModelConfig.World.bottom:
@@ -271,12 +372,12 @@ class InclinedLayer(ResObject):
                 verts = [upper_line_pt1, rb_pt, upper_line_pt2]
             else:
                 lower_line_pt1 = (ModelConfig.World.right, bottom_value)
-                lower_line_pt2 = self._calc_opposite_point_with_angle(depth, lower_line_pt1, angle)
+                lower_line_pt2 = self._calc_opposite_point_with_angle(lower_line_pt1, angle)
                 verts = [upper_line_pt1, upper_line_pt2, lower_line_pt2, lower_line_pt1]
 
         elif -90 <= angle < 0:
             upper_line_pt1 = (ModelConfig.World.left, depth)
-            upper_line_pt2 = self._calc_opposite_point_with_angle(depth, upper_line_pt1, angle)
+            upper_line_pt2 = self._calc_opposite_point_with_angle(upper_line_pt1, angle)
             bottom_value = depth + height / np.cos(np.deg2rad(angle))
 
             if bottom_value > ModelConfig.World.bottom:
@@ -284,7 +385,7 @@ class InclinedLayer(ResObject):
                 verts = [upper_line_pt1, lb_pt, upper_line_pt2]
             else:
                 lower_line_pt1 = (ModelConfig.World.left, bottom_value)
-                lower_line_pt2 = self._calc_opposite_point_with_angle(depth, lower_line_pt1, angle)
+                lower_line_pt2 = self._calc_opposite_point_with_angle(lower_line_pt1, angle)
                 verts = [upper_line_pt1, upper_line_pt2, lower_line_pt2, lower_line_pt1]
         else:
             raise ValueError("Angle must be between -90 and 90")
@@ -295,6 +396,10 @@ class InclinedLayer(ResObject):
 
 class PGMeshCreator:
     def __init__(self, resobjects_list: Union[ResObject, List[ResObject]]):
+        """
+        The class allows you to combine different primitives on one pygimli Mesh.
+        Primitives must inherit from class ResObject, have a polygon and a marker.
+        """
         self.objects_list = None
         self.plc = None
 
@@ -302,12 +407,20 @@ class PGMeshCreator:
             self.compose_objects(resobjects_list)
 
     def compose_objects(self, resobjects_list: Union[ResObject, List[ResObject]]):
+        """
+        The method combines primitives and puts them on one base.
+
+        NOTE:
+            The primitives are stacked on top of each other with overlap.
+            The number of formed polygons is equal to or greater than the number of the original primitives.
+        """
         if isinstance(resobjects_list, ResObject):
             resobjects_list = [resobjects_list]
 
         world_polygon = Polygon(ResObject.get_world_pts())
 
         world = gpd.GeoDataFrame({'geometry': world_polygon, 'marker': [0]})
+        assert resobjects_list[0].marker is not None
         first_poly = gpd.GeoDataFrame({'geometry': resobjects_list[0].get_polygon(),
                                       'marker': [resobjects_list[0].marker]})
 
@@ -315,6 +428,7 @@ class PGMeshCreator:
 
         if len(resobjects_list) > 1:
             for extra_poly in resobjects_list[1:]:
+                assert extra_poly.marker is not None
                 extra_poly = gpd.GeoDataFrame({'geometry': extra_poly.get_polygon(),
                                                'marker': [extra_poly.marker]})
                 res_union = self.merge_two_polygons(res_union, extra_poly)
@@ -333,6 +447,11 @@ class PGMeshCreator:
 
     @staticmethod
     def merge_two_polygons(background_polygons, extra_polygon):
+        """
+        The method looks for the union of polygons of two frames and, based on them, makes a series of new polygons.
+        If any final polygon contains the value of the upper polygon, then the marker of this polygon is assigned,
+        otherwise the marker of the previous polygon.
+        """
         res_union = gpd.overlay(background_polygons, extra_polygon, how='union')
 
         res_polygons = []
